1755de795a7d222d300ccc506777014ff91be021
Dohyeon <michaia0363@gmail.com>
2025-02-09 00:58:22 +0900

    git ignore 추가 및 git lfs prune 테스트

--- MOLES/Assets/Scripts/MapCreatingTest.cs
+++ MOLES/Assets/Scripts/MapCreatingTest.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b9aab49c02b6186b04187680544f742624325ca9e711cd27895fd40a2c16d04
-size 4310
+oid sha256:fbaecfd2f48c2ba75d003a7e308f211bb0649bb8adbc5391f806c81906213a0e
+size 4469
