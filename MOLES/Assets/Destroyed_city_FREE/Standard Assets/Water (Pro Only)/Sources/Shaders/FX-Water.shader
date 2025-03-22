d9ace5869564c2d99232ae8ec89f0658dcf46fa8
Dohyeon <michaia0363@gmail.com>
2025-03-22 09:12:33 +0900

    집에서 파일 연동

--- MOLES/Assets/Destroyed_city_FREE/Standard Assets/Water (Pro Only)/Sources/Shaders/FX-Water.shader
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e8a6efac907b45ae8137e51f198f666d2c63084574900a9faf85184c6f9a9493
-size 5295
--- MOLES/Assets/Scripts/PlayerController.cs
+++ MOLES/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6baba555e50d78ef184d4e9c6090ccdc54d5ca4ce95dfcada4b383d48b6e4071
-size 4772
+oid sha256:5aa0fe34d64fd9ae9e4034ee418e315e461ea52f6f86a5795a06c414b421ada5
+size 4774
