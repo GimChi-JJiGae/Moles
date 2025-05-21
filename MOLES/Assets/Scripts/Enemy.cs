f49818def9413ebbc1d3b13aeea407a9ac66f36c
michaia0363 <michaia0363@gmail.com>
2025-05-21 23:18:29 +0900

    노멀맵 문제해결 중, 다시 에셋 임포트필요

--- MOLES/Assets/Scripts/Enemy.cs
+++ MOLES/Assets/Scripts/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7216326e78e7e7b6af6c922faa21e0dd4a1c9130b3ef822c7ac3b86115213551
-size 17411
+oid sha256:0a1e339d0dd4b1bdf419f39bb7db148a07f92d2037f05b720b4be70c79e90511
+size 17654
