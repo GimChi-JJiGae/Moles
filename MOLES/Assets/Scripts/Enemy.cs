4b86200321bc6ea9440d60f1fc1ead2b326eced9
michaia0363 <michaia0363@gmail.com>
2025-05-21 23:05:26 +0900

    자취방에서 프로젝트 다시 셋팅
    노멀맵이 생성될 떄 빨갛게 뜨는 문제확인중, RenderTexture문제인듯

--- MOLES/Assets/Scripts/Enemy.cs
+++ MOLES/Assets/Scripts/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b85c815aebbb2f2f11a2258d25d16abd536b2e9442b14eafd49025de88edf13
-size 17367
+oid sha256:7216326e78e7e7b6af6c922faa21e0dd4a1c9130b3ef822c7ac3b86115213551
+size 17411
