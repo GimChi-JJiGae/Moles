68faa480684929ef3e7fcf07fe441bda5cd7bc53
Dohyeon <michaia0363@gmail.com>
2025-05-24 17:25:05 +0900

    노멀맵 문제 연구

--- MOLES/Assets/Scripts/Enemy.cs
+++ MOLES/Assets/Scripts/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5632fa5c8bd6ff83b6ed14d44eef422f1524bc6cd9314cff24181dde4011305
-size 17842
+oid sha256:59acae3edd1db5881ee7fd0f8693ec3a471106ced8f869ed4c221042f2ce0f59
+size 19563
---
+++ MOLES/Assets/Scripts/RenderTextureSaver.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e05cd71ac52d7eeb79d350dacfc1cf11c3b7546578439e75f7c2475c374e9e27
+size 966
