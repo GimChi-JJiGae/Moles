96314a753b16e8920a805ac8455d68123fa51d5e
Dohyeon <michaia0363@gmail.com>
2025-06-28 17:26:54 +0900

    정찰 기능, 피격 죽음 기능, 피격 시 공격시작 추가
    공격시 트리거로 공격 모션 나오는 기능이 제대로 동작하지 않는 중

--- MOLES/Assets/Scripts/Enemy.cs
+++ MOLES/Assets/Scripts/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59acae3edd1db5881ee7fd0f8693ec3a471106ced8f869ed4c221042f2ce0f59
-size 19563
+oid sha256:8ec4b7470d5e2b537ba589f59812dd3a96a9758d9fe824abea44036293600a44
+size 23560
