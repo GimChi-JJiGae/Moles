6e2429649a11b665d19d1ec8241e06cfb4d29dc7
Dohyeon <michaia0363@gmail.com>
2025-08-16 17:30:30 +0900

    아이템 줍기 완료. 맵 형태 구체적 개발중

--- MOLES/Assets/Scripts/Enemy.cs
+++ MOLES/Assets/Scripts/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2273eef5a9b4cf0a5a0b422ed8d1cd7d237024ae5970b5e2d15030540df4e20
-size 28035
+oid sha256:5d70070f7acef4a14e8ec5a1082b2e459e881530859882becd6969fa575471a8
+size 28326
---
+++ MOLES/Assets/Scripts/Item.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:00038555c4043606451b404e0bb291bd2f2365581bb900cef5a9b93ebf4c204a
+size 2693
