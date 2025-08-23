c7e07a084ffde51a1f7eb8cd5ba18ae3c8e36bf7
Dohyeon <michaia0363@gmail.com>
2025-08-23 17:26:41 +0900

    문에 여닫는 기능 추가, 문 콜라이더 수정
    문을 다음 방이 있는 위치에 잡아주는게 중요할 것

---
+++ MOLES/Assets/Scripts/DoorControl.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e0ddc7076ca3edac68bea67a33df445f95502327738971af34d08b84405bbdc2
+size 1823
--- MOLES/Assets/Scripts/RoomCreator.cs
+++ MOLES/Assets/Scripts/RoomCreator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ddac0a697d3c07b653029296be38367e16ccdc90cfb0b6ac8837df14dce86c1
-size 17245
+oid sha256:52c9f9cf85a0b8629c6418a90bbebd65f267cc3b183d6b3eaed7c9bc4fc4fcc4
+size 17251
