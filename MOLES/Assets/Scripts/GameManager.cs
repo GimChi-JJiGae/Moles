fbdfd0844d8aa985c97e84fe0ef942530371ef1f
Dohyeon <michaia0363@gmail.com>
2025-10-25 17:31:16 +0900

    시작 방, 끝 방 집어넣기 완료

--- MOLES/Assets/Scripts/GameManager.cs
+++ MOLES/Assets/Scripts/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccb473e9bc05a72882dd4bb53a179f4758c33ae55647276609bf0cf979a5184b
-size 1054
+oid sha256:3ec16686cc7e83ddcf31ff0acda5de1d4da54c0d2ba962ebe7aa048dff96a621
+size 1130
--- MOLES/Assets/Scripts/RoomCreator.cs
+++ MOLES/Assets/Scripts/RoomCreator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d212eb8ee69c841dedbceb5828396addb15e8958197fd330e31a49f49d9e4512
-size 27600
+oid sha256:784ea9bd62da100133b97ba11025ab5bcf8629503fa420dca85ae4c4ce8be142
+size 29891
