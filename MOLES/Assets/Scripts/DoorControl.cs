5b095ab38218131fcc0623fcdd6dd170b4db4f8b
Dohyeon <michaia0363@gmail.com>
2025-11-01 17:22:19 +0900

    방 규격사이즈 맞추기
    문 벽의 (특히 대형 정사각형방) 크기 조금 늘려주기

--- MOLES/Assets/Scripts/DoorControl.cs
+++ MOLES/Assets/Scripts/DoorControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0ddc7076ca3edac68bea67a33df445f95502327738971af34d08b84405bbdc2
-size 1823
+oid sha256:294a6e73665d58f9862fcf77ef88541fbb664326b6ee3a3fdf49dc0d73803614
+size 1825
--- MOLES/Assets/Scripts/RoomCreator.cs
+++ MOLES/Assets/Scripts/RoomCreator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:784ea9bd62da100133b97ba11025ab5bcf8629503fa420dca85ae4c4ce8be142
-size 29891
+oid sha256:3126592fe90c25e4e8e08571cc33a10cba2dc824a5e95a693a4e919841ccb165
+size 29961
