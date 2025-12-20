9756b39b8d177eccc1fa431a2dc05d17965d021c
Dohyeon <michaia0363@gmail.com>
2025-12-20 17:23:27 +0900

    캐릭터 충돌감지 재설정
    Rigidbody의 rb.MovePosition이 잘 안됨. Transform.rotation 이 변경되고있어서그럼. Raycast로 처리할것

--- MOLES/Assets/Scripts/PlayerController.cs
+++ MOLES/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be51c8178e232a6f4dd44659fd19182de0ce9c4b01142cad52726ac192c0482e
-size 24635
+oid sha256:015cbe03da9280097bed62c6b705aac352dfaf8293a7158d150e1a916dc6e119
+size 25018
