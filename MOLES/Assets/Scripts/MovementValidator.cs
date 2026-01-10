437d1675478a16f5a60735bc2817a9abcd1f2a75
Dohyeon <michaia0363@gmail.com>
2026-01-10 17:22:23 +0900

    충돌, 오르막 해결 완료. 점프 기능 구현 중
    바닥이 강제로 감지되는 것과 점프키가 씹히는 것 때문에 jumpstarttime을 넣고 update에 키입력을 넣음. 그런데 현재 점프 후 바닥을 제대로 감지 못하는지 바닥뚫음

--- MOLES/Assets/Scripts/MovementValidator.cs
+++ MOLES/Assets/Scripts/MovementValidator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f90d535cc19be06a0c4c781d143de00c3ba71bfa754026ecd0f02f7954ec5d8a
-size 3130
+oid sha256:a59abd3162f940f3e8030fd496e25dbe008570433e5b2f1f9fdf3a75c07ab074
+size 4111
--- MOLES/Assets/Scripts/PlayerController.cs
+++ MOLES/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d10c80a3b432dd897f97d44c2e7d2663f6fe5f91d8da5203c74bda98c2784d5e
-size 26929
+oid sha256:c08afcfd8a24bd95f47455b7cf93e15a15b419092e91b5ae506a53a17c09d032
+size 30101
