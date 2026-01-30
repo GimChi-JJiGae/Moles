8f0bda43b301654c9ad6ec48129d5f9b9990cf3d
michaia0363 <michaia0363@gmail.com>
2026-01-30 17:34:40 +0900

    점프시 바닥이 지나쳐지는 문제 수정중

--- MOLES/Assets/Scripts/PlayerController.cs
+++ MOLES/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c08afcfd8a24bd95f47455b7cf93e15a15b419092e91b5ae506a53a17c09d032
-size 30101
+oid sha256:520659299e276d26044c2f97a8234339e6daf9d8e4a632c007a4ad68f356fb82
+size 30120
