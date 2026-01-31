a8b601c68bb1e36d4b54ff4e0601ca1c2db8e0d2
Dohyeon <michaia0363@gmail.com>
2026-01-31 17:17:15 +0900

    적 공격 방식 업데이트 준비
    적이 주변에 도달한 후 일정거리에서 돌진공격을 할 수 있도록, 플레이어 주변 지점을 좌표로 확보

---
+++ MOLES/Assets/Scripts/EnemyCommon.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:96fc69af28108218f205ee2dc6a80e763afc2f311a00a12170b002402ad77baa
+size 1798
---
+++ MOLES/Assets/Scripts/PlayerSurroundPositions.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:323eb36e219c1d8af754bbd521f294e29eebce42014c384d589abd1abad792be
+size 1605
