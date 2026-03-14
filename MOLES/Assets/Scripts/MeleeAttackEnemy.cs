76598f7e57f74fec9e7f8ea5523161d35b8af170
Dohyeon <michaia0363@gmail.com>
2026-03-14 17:28:55 +0900

    공격 위치까지 도달가능
    그런데 시야에 안보여서 다시 공격위치를 잡는 방법은 어떻게 해야할까. 일단 브루트포스밖에 생각이 안남

--- MOLES/Assets/Scripts/MeleeAttackEnemy.cs
+++ MOLES/Assets/Scripts/MeleeAttackEnemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9aaa95d7613da1e4dfde1191dd725d93b8c5830146ac3926591b0ed30fd0e0f1
-size 6977
+oid sha256:e27a026e77da85805e59f28951b8533bb5f59d476bc9dd8fbacc79a98d561671
+size 9126
--- MOLES/Assets/Scripts/MovementValidator.cs
+++ MOLES/Assets/Scripts/MovementValidator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a59abd3162f940f3e8030fd496e25dbe008570433e5b2f1f9fdf3a75c07ab074
-size 4111
+oid sha256:b5dc2ed5738a5a4c83976bc2e6490b7ca6fa8623f398e8c0535fd492bb4f2749
+size 4103
