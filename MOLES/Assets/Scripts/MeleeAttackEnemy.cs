408a409384d63978c945610fa4db4898031e50f8
Dohyeon <michaia0363@gmail.com>
2026-03-21 17:27:45 +0900

    공격 목표까지 이동 후 Roar단계 진입
    점프 공격에 대해서 구현해야하는데 캐릭터 컨트롤러라는 것을 사용할 수 있다고 함. 조사필요

--- MOLES/Assets/Scripts/MeleeAttackEnemy.cs
+++ MOLES/Assets/Scripts/MeleeAttackEnemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e27a026e77da85805e59f28951b8533bb5f59d476bc9dd8fbacc79a98d561671
-size 9126
+oid sha256:f118d35ab00ce79d94cbf1e2aea7bdadccb9345353f0367d20f6a077b45f5f28
+size 10412
--- MOLES/Assets/Scripts/MovementValidator.cs
+++ MOLES/Assets/Scripts/MovementValidator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5dc2ed5738a5a4c83976bc2e6490b7ca6fa8623f398e8c0535fd492bb4f2749
-size 4103
+oid sha256:c0471f460983ae853c6f02f1ac36f7c65341c4b823f9c593e4f3a6a9718180e6
+size 4097
