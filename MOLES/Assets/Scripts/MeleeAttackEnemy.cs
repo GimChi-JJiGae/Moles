dc6881984a54134d1c8956fe380541f62bb9d892
Dohyeon <michaia0363@gmail.com>
2026-04-11 17:26:13 +0900

    점프 공격 구현 중간단계 완료
    점프해서 공격까진 하나, 초기 자리 잡는 알고리즘을 단순화 + 빠르게 자리잡게 해야함. 그 후 근접 공격 처리 필요

--- MOLES/Assets/Scripts/MeleeAttackEnemy.cs
+++ MOLES/Assets/Scripts/MeleeAttackEnemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f118d35ab00ce79d94cbf1e2aea7bdadccb9345353f0367d20f6a077b45f5f28
-size 10412
+oid sha256:2d9020759230af1f2faa7413f23df3db11cd581545770e2e35944883a850315c
+size 14190
